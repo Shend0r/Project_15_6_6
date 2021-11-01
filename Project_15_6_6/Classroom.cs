32df4e6d10376a2998a513306d410e8fca502788
kolop <kolop@SHANDOR>
2021-11-01 18:41:05 +0300

    Добавьте файлы проекта.

---
+++ Project_15_6_6/Classroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_15_6_6
+{
+    internal class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Project_15_6_6/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Project_15_6_6;
+
+var classes = new[]
+{
+     new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+     new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+     new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+};
+
+var allStudents = GetAllStudents(classes);
+
+foreach (var student in allStudents)
+{
+    Console.WriteLine(student);
+}
+
+static List<string> GetAllStudents(Classroom[] classes)
+{
+    List<string> allStudents = new List<string>();
+
+    foreach (var student in classes[0].Students)
+    {
+        allStudents.Add(student);
+    }
+
+    foreach (var student in classes[1].Students)
+    {
+        allStudents.Add(student);
+    }
+
+    foreach (var student in classes[2].Students)
+    {
+        allStudents.Add(student);
+    }
+
+    return allStudents;
+}
